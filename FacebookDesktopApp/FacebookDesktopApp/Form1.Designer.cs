01053fdadb441143c8b194d9f563fb829a4ecdc0
Ben Bublil <benbu@mta.ac.il>
2019-03-14 15:45:30 +0200

    set up

---
+++ FacebookDesktopApp/FacebookDesktopApp/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace FacebookDesktopApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.buttonLogin = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBoxProfilePic = new System.Windows.Forms.PictureBox();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.buttonLogin)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxProfilePic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(103)))), ((int)(((byte)(176)))));
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1219, 47);
+            this.panel2.TabIndex = 1;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::FacebookDesktopApp.Properties.Resources.logo;
+            this.pictureBox1.Location = new System.Drawing.Point(6, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(248, 43);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Open Sans Hebrew", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.textBox1.Location = new System.Drawing.Point(453, 86);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(577, 37);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "Welcome to FacebookDesktop Application";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Open Sans Hebrew", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(487, 139);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(482, 29);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "For full access to this application, you need to log in first.";
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.ErrorImage = global::FacebookDesktopApp.Properties.Resources.login;
+            this.buttonLogin.Image = global::FacebookDesktopApp.Properties.Resources.login;
+            this.buttonLogin.Location = new System.Drawing.Point(598, 196);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(232, 51);
+            this.buttonLogin.TabIndex = 4;
+            this.buttonLogin.TabStop = false;
+            this.buttonLogin.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(49)))), ((int)(((byte)(71)))));
+            this.panel1.Controls.Add(this.pictureBoxProfilePic);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 47);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(254, 570);
+            this.panel1.TabIndex = 5;
+            // 
+            // pictureBoxProfilePic
+            // 
+            this.pictureBoxProfilePic.Location = new System.Drawing.Point(12, 16);
+            this.pictureBoxProfilePic.Name = "pictureBoxProfilePic";
+            this.pictureBoxProfilePic.Size = new System.Drawing.Size(80, 80);
+            this.pictureBoxProfilePic.TabIndex = 6;
+            this.pictureBoxProfilePic.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1219, 617);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.buttonLogin);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Facebook Desktop";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.buttonLogin)).EndInit();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxProfilePic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.PictureBox buttonLogin;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBoxProfilePic;
+    }
+}
+
---
+++ FacebookDesktopApp/FacebookDesktopApp/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FacebookWrapper.ObjectModel;
+using FacebookWrapper;
+
+namespace FacebookDesktopApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        User m_LoggedInUser;
+
+        private void loginAndInit()
+        {
+            /// Owner: design.patterns
+
+            /// Use the FacebookService.Login method to display the login form to any user who wish to use this application.
+            /// You can then save the result.AccessToken for future auto-connect to this user:
+            LoginResult result = FacebookService.Login("1450160541956417", /// (desig patter's "Design Patterns Course App 2.4" app)
+
+                "public_profile",
+                "email",
+                "publish_to_groups",
+                "user_birthday",
+                "user_age_range",
+                "user_gender",
+                "user_link",
+                "user_tagged_places",
+                "user_videos",
+                "publish_to_groups",
+                "groups_access_member_info",
+                "user_friends",
+                "user_events",
+                "user_likes",
+                "user_location",
+                "user_photos",
+                "user_posts",
+                "user_hometown"
+
+                /// DEPRECATED PERMISSIONS:
+                ///"publish_actions"
+                ///"user_about_me",
+                ///"user_education_history",
+                ///"user_actions.video",
+                ///"user_actions.news",
+                ///"user_actions.music",
+                ///"user_actions.fitness",
+                ///"user_actions.books",
+                ///"user_games_activity",
+                ///"user_managed_groups",
+                ///"user_relationships",
+                ///"user_relationship_details",
+                ///"user_religion_politics",
+                ///"user_tagged_places",
+                ///"user_website",
+                ///"user_work_history",
+                ///"read_custom_friendlists",
+                ///"read_page_mailboxes",
+                ///"manage_pages",
+                ///"publish_pages",
+                ///"publish_actions",
+                ///"rsvp_event"
+                ///"user_groups" (This permission is only available for apps using Graph API version v2.3 or older.)
+                ///"user_status" (This permission is only available for apps using Graph API version v2.3 or older.)
+                /// "read_mailbox", (This permission is only available for apps using Graph API version v2.3 or older.)
+                /// "read_stream", (This permission is only available for apps using Graph API version v2.3 or older.)
+                /// "manage_notifications", (This permission is only available for apps using Graph API version v2.3 or older.)
+
+                );
+            /// The documentation regarding facebook login and permissions can be found here: 
+            // https://developers.facebook.com/docs/facebook-login/permissions#reference
+
+
+            if (!string.IsNullOrEmpty(result.AccessToken))
+            {
+                m_LoggedInUser = result.LoggedInUser;
+                fetchUserInfo();
+                
+            }
+            else
+            {
+                MessageBox.Show(result.ErrorMessage);
+            }
+        }
+
+        private void fetchUserInfo()
+        {
+            picture_smallPictureBox.LoadAsync(m_LoggedInUser.PictureNormalURL);
+            if (m_LoggedInUser.Posts.Count > 0)
+            {
+                textBoxStatus.Text = m_LoggedInUser.Posts[0].Message;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
