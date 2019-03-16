9c5afa00071a7f924b382f878d39e6df66609d00
danakeydar <dana260893@gmail.com>
2019-03-16 18:31:41 +0200

    new

--- B19 EX01 Ben 305401317 Dana 311358543/B19 EX01 Ben 305401317 Dana 311358543/Form1.cs
+++ B19 EX01 Ben 305401317 Dana 311358543/B19 EX01 Ben 305401317 Dana 311358543/Form1.cs
@@ -14,6 +14,8 @@ namespace B19_EX01_Ben_305401317_Dana_311358543
 {
     public partial class Form1 : Form
     {
+        private FacebookDesktopLogic m_FacebookDesktopLogic = FacebookDesktopLogic.GetFacebookDesktopLogic();
+
         public Form1()
         {
             InitializeComponent();
@@ -56,79 +58,25 @@ namespace B19_EX01_Ben_305401317_Dana_311358543
 
         private void pictureBox4_Click_1(object sender, EventArgs e)
         {
-            loginAndInit();
-        }
-        private void loginAndInit()
-        {
-            /// Owner: design.patterns
-
-            /// Use the FacebookService.Login method to display the login form to any user who wish to use this application.
-            /// You can then save the result.AccessToken for future auto-connect to this user:
-            LoginResult result = FacebookService.Login("1450160541956417", /// (desig patter's "Design Patterns Course App 2.4" app)
-
-                "public_profile",
-                "email",
-                "publish_to_groups",
-                "user_birthday",
-                "user_age_range",
-                "user_gender",
-                "user_link",
-                "user_tagged_places",
-                "user_videos",
-                "publish_to_groups",
-                "groups_access_member_info",
-                "user_friends",
-                "user_events",
-                "user_likes",
-                "user_location",
-                "user_photos",
-                "user_posts",
-                "user_hometown"
-
-                /// DEPRECATED PERMISSIONS:
-                ///"publish_actions"
-                ///"user_about_me",
-                ///"user_education_history",
-                ///"user_actions.video",
-                ///"user_actions.news",
-                ///"user_actions.music",
-                ///"user_actions.fitness",
-                ///"user_actions.books",
-                ///"user_games_activity",
-                ///"user_managed_groups",
-                ///"user_relationships",
-                ///"user_relationship_details",
-                ///"user_religion_politics",
-                ///"user_tagged_places",
-                ///"user_website",
-                ///"user_work_history",
-                ///"read_custom_friendlists",
-                ///"read_page_mailboxes",
-                ///"manage_pages",
-                ///"publish_pages",
-                ///"publish_actions",
-                ///"rsvp_event"
-                ///"user_groups" (This permission is only available for apps using Graph API version v2.3 or older.)
-                ///"user_status" (This permission is only available for apps using Graph API version v2.3 or older.)
-                /// "read_mailbox", (This permission is only available for apps using Graph API version v2.3 or older.)
-                /// "read_stream", (This permission is only available for apps using Graph API version v2.3 or older.)
-                /// "manage_notifications", (This permission is only available for apps using Graph API version v2.3 or older.)
-
-                );
-            /// The documentation regarding facebook login and permissions can be found here: 
-            // https://developers.facebook.com/docs/facebook-login/permissions#reference
-
-
-            if (!string.IsNullOrEmpty(result.AccessToken))
+            bool isLogIn = m_FacebookDesktopLogic.LoginAndInit();
+            if(isLogIn)
             {
-                m_LoggedInUser = result.LoggedInUser;
                 fetchUserInfo();
             }
             else
             {
-                MessageBox.Show(result.ErrorMessage);
+                MessageBox.Show("Error!"/*result.ErrorMessage*/); //todo: result?
             }
         }
 
+        private void fetchUserInfo()
+        {
+            pictureBox2.LoadAsync(m_FacebookDesktopLogic.LoggedInUser.PictureNormalURL);
+            textBox2.Text = m_FacebookDesktopLogic.LoggedInUser.Name;
+            //if (m_FacebookDesktopLogic.LoggedInUser.Posts.Count > 0)
+            //{
+            //    textBoxStatus.Text = m_FacebookDesktopLogic.LoggedInUser.Posts[0].Message;
+            //}
+        }
     }
 }
---
+++ B19 EX01 Ben 305401317 Dana 311358543/FacebookDesktopLogic/FacebookDesktopLogic.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FacebookWrapper;
+using FacebookWrapper.ObjectModel;
+
+namespace B19_EX01_Ben_305401317_Dana_311358543
+{
+    public class FacebookDesktopLogic
+    {
+        private bool m_IsLogIn = false;
+
+        private static FacebookDesktopLogic s_FacebookDesktopLogic = null;
+        private User m_LoggedInUser;
+
+        public User LoggedInUser
+        {
+            get { return m_LoggedInUser; }
+        }
+
+        private FacebookDesktopLogic() { }
+        public static FacebookDesktopLogic GetFacebookDesktopLogic()
+        {
+            //todo: lock
+            if (s_FacebookDesktopLogic == null)
+            {
+                s_FacebookDesktopLogic = new FacebookDesktopLogic();
+            }
+
+            return s_FacebookDesktopLogic;
+        }
+
+        public bool LoginAndInit()
+        {
+            /// Owner: design.patterns
+
+            /// Use the FacebookService.Login method to display the login form to any user who wish to use this application.
+            /// You can then save the result.AccessToken for future auto-connect to this user:
+            LoginResult result = FacebookService.Login("352758402005372", /// (desig patter's "Design Patterns Course App 2.4" app)
+
+                "public_profile",
+                "email",
+                "publish_to_groups",
+                "user_birthday",
+                "user_age_range",
+                "user_gender",
+                "user_link",
+                "user_tagged_places",
+                "user_videos",
+                "publish_to_groups",
+                "groups_access_member_info",
+                "user_friends",
+                "user_events",
+                "user_likes",
+                "user_location",
+                "user_photos",
+                "user_posts",
+                "user_hometown"
+
+                /// DEPRECATED PERMISSIONS:
+                ///"publish_actions"
+                ///"user_about_me",
+                ///"user_education_history",
+                ///"user_actions.video",
+                ///"user_actions.news",
+                ///"user_actions.music",
+                ///"user_actions.fitness",
+                ///"user_actions.books",
+                ///"user_games_activity",
+                ///"user_managed_groups",
+                ///"user_relationships",
+                ///"user_relationship_details",
+                ///"user_religion_politics",
+                ///"user_tagged_places",
+                ///"user_website",
+                ///"user_work_history",
+                ///"read_custom_friendlists",
+                ///"read_page_mailboxes",
+                ///"manage_pages",
+                ///"publish_pages",
+                ///"publish_actions",
+                ///"rsvp_event"
+                ///"user_groups" (This permission is only available for apps using Graph API version v2.3 or older.)
+                ///"user_status" (This permission is only available for apps using Graph API version v2.3 or older.)
+                /// "read_mailbox", (This permission is only available for apps using Graph API version v2.3 or older.)
+                /// "read_stream", (This permission is only available for apps using Graph API version v2.3 or older.)
+                /// "manage_notifications", (This permission is only available for apps using Graph API version v2.3 or older.)
+
+                );
+            /// The documentation regarding facebook login and permissions can be found here: 
+            // https://developers.facebook.com/docs/facebook-login/permissions#reference
+
+
+            if (!string.IsNullOrEmpty(result.AccessToken))
+            {
+                m_LoggedInUser = result.LoggedInUser;
+                this.m_IsLogIn = true;
+                //fetchUserInfo();
+            }
+            else
+            {
+                this.m_IsLogIn = false;
+                //MessageBox.Show(result.ErrorMessage);
+            }
+
+            return m_IsLogIn;
+        }
+
+    }
+}
